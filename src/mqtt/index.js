c03834805ddba379d8040d78d8f4a4686867bd9d
Afaq Javed <afaqjavedofficial@gmail.com>
2021-11-22 18:14:18 +0500

    added

--- src/mqtt/index.js
+++ src/mqtt/index.js
@@ -1,21 +1,23 @@
-// import * as mqtt from "mqtt";
-// let client = mqtt.connect("ws://192.46.209.78:1886/ws"); // create a client
+import * as mqtt from "mqtt";
+let client = mqtt.connect("mqtts://192.46.209.78:1887/ws", {
+  rejectUnauthorized: false,
+}); // create a client
 
-// client.on("connect", function () {
-//   client.subscribe("presence", function (err) {
-//     if (!err) {
-//       //   client.publish("presence", "Hello mqtt");
-//       client.subscribe("test");
-//     } else {
-//       console.log(err);
-//     }
-//   });
-// });
+client.on("connect", function () {
+  client.subscribe("presence", function (err) {
+    if (!err) {
+      //   client.publish("presence", "Hello mqtt");
+      client.subscribe("test");
+    } else {
+      console.log(err);
+    }
+  });
+});
 
-// client.on("message", function (topic, message) {
-//   // message is Buffer
-//   console.log("Topic  : " + topic + "     " + message.toString());
-//   //   console.log(message.toString());
-// });
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log("Topic  : " + topic + "     " + message.toString());
+  //   console.log(message.toString());
+});
 
-// export default client;
+export default client;
