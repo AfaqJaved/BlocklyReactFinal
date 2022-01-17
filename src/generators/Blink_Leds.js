c70e608099add3a10620ee2f5711ee7eb3cd87b4
AfaqJaved <afaqjavedofficial@gmail.com>
2022-01-17 17:08:32 +0500

    ttest

--- src/generators/Blink_Leds.js
+++ src/generators/Blink_Leds.js
@@ -1,18 +1,5 @@
 import * as Blockly from "blockly";
 
-// Blockly.JavaScript["blink_leds"] = function (block) {
-//   var dropdown_color = block.getFieldValue("color");
-//   var number_loop = block.getFieldValue("loop");
-
-//   var code =
-//     " await SMARTY.blinkLeds('" +
-//     dropdown_color.toString() +
-//     "' , " +
-//     number_loop +
-//     ")\n";
-//   return code;
-// };
-
 Blockly.JavaScript["blink_rgb_leds"] = function (block) {
   var dropdown_color = block.getFieldValue("color");
   var number_loop = block.getFieldValue("loop");
