4427b7a64dd595752075987d37bc7b2eb2e36563
AfaqJaved <afaqjavedofficial@gmail.com>
2022-05-26 12:55:56 +0500

    added mqtt check

--- src/mqtt/index.js
+++ src/mqtt/index.js
@@ -4,24 +4,22 @@ let client = mqtt.connect("mqtts://mqtt.afaqjaved.com", {
   port: 1885,
   username: "afaq",
   password: "test",
+  // clientId: "blockly",
+  // path: "/ws",
+  // protocol: "mqtts",
+  // protocolId: "MQTT",
+  // protocolVersion: 3,
+  // rejectUnauthorized: false,
 }); // create a client
 
 client.on("connect", function () {
-  client.subscribe("presence", function (err) {
-    if (!err) {
-      //   client.publish("presence", "Hello mqtt");
-      client.subscribe("test");
-      client.publish("test", "working");
-    } else {
-      console.log(err);
-    }
-  });
+  client.subscribe("test");
+  client.publish("test", "working");
+  console.log("connected");
 });
 
-// client.on("message", function (topic, message) {
-//   // message is Buffer
-//   console.log("Topic  : " + topic + "     " + message.toString());
-//   //   console.log(message.toString());
-// });
+client.on("message", function (topic, message) {
+  console.log("Topic  : " + topic + "     " + message.toString());
+});
 
 export default client;
